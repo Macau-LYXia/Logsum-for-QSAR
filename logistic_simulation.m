0917df16dbd10eee95c0ed7a0f5d23e1b59b7e48
e <xia2yin1234@gmail.com>
2018-10-10 20:31:11 +0800

    first commit

---
+++ logistic_simulation.m
@@ -0,0 +1,131 @@
+clear
+clc
+
+%%%%%%%%%% generate benchmark dataset %%%%%%%%%%%%%%%%%%%%%%%%
+beta=zeros(1,1000);
+beta(1)=1;
+beta(2)=1;
+beta(3)=-1;
+beta(4)=-1;
+beta(5)=1;
+beta(6)=1;
+beta(7)=-1;
+beta(8)=1;
+beta(9)=-1;
+beta(10)=1;
+actual_beta=beta';
+%%%%%%%%the size of train and test%%%%
+sample_size=200;
+test_size=floor(sample_size*0.3);
+train_size=sample_size-test_size;
+%%%%%%%%      B0  %%%%%%%%%%%%%%%
+intercept=0.0;
+
+%%%%% produce the data%%%%
+X = normrnd(0, 1, sample_size, size(beta,2)+1);
+[n,p]=size(X)
+% correlation
+cor=0.2
+for i=2:5
+    X(:,i) = cor*(X(:,1))+ (1-cor)*(X(:,i));
+end
+x=X(:,1:p-1);
+%%%%control signla %%%%
+l=intercept+(x*beta'+0.3*normrnd(0, 1, n, 1));
+y=sigmoid(l);
+%%%%%rand the sort of sample
+R = randperm(n);
+
+%%%%% divid training and test %%%%
+x_test = x(R(1:test_size),:);
+y_test=y(R(1:test_size),:);
+R(1:test_size)=[];
+x_train=x(R,:);
+y_train=y(R,:);
+
+%%%%%%%%% B0 %%%%%%%%%%%%
+[row, col] = size(x_train);
+temp=sum(y_train)/row;
+beta_zero=log(temp/(1-temp));    %intercept
+beta=zeros(col,1);
+
+%%%%%%%%%%%%%% compute lambda on the log scale %%%%%%%%%%%%%%%%%%%%%
+eta=beta_zero;   %%%%% eta= intercept + X*beta;
+Pi=exp(eta)./(1+exp(eta));
+W=diag(Pi.*(1-Pi));           %%%%%%%%% W is diagonal matrix%%%%%%%%%%
+r=(W^-1)*(y_train-Pi);            %residual= (w^-1)*(y_train-pi)
+S=(x_train'*W*r)/row;
+
+lambda_max=(4/3*(max(S)))^(1.5);
+lambda_min = lambda_max*0.00005;
+m =10;
+
+for i=1:m
+    Lambda(i) = lambda_max*(lambda_min/lambda_max)^(i/m);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i=1:m
+    lambda=Lambda(i);
+    iter=0;
+    maxiter=1000;
+    beta_path(:,i)=beta(:,1);
+    while iter<maxiter %true
+        beta_temp=beta;
+        beta_zero_temp=beta_zero;
+        eta=beta_zero_temp+x_train*beta_temp;   %%%%% eta= intercept + X*beta;
+        Pi=exp(eta)./(1+exp(eta));
+        W=diag(Pi.*(1-Pi));           %%%%%%%%% W is diagonal matrix%%%%%%%%%%
+        r=(W^-1)*(y_train-Pi);            %residual= (w^-1)*(y_train-pi)
+        
+        %%%%%%%%%%%%%%%%%%%% intercept%%%%%%%%%%%%%%%%%%%%%%
+        beta_zero=sum(W*r)/sum(sum(W))+beta_zero_temp;    %%%
+        r=r-(beta_zero-beta_zero_temp);                %%%%%%%%%%%%%%%%%%%%%%
+        for j=1:col
+            v=x_train(:,j)'*W*x_train(:,j)/row;
+            S=(x_train(:,j)'*W*r)/row+beta_temp*v;           %%%%%%% v is weight
+            
+            %%%%%%%%%%%%%%%%%% Half Thresholding(Xu,et al 2010) %%%%%%%%%%%%%%%%%
+            
+            %             if abs(S(j)) > ((3/4)*(lambda^(2/3)))
+            %                 phi = acos(lambda/8*((abs(S(j))/3)^(-1.5)));
+            %                 beta(j) = real(2/3*S(j)*(1 + cos(2/3*(pi - phi))))/v; %%%%%%%pi=3.14135....
+            %             else
+            %                 beta(j) = 0;
+            %             end
+            %
+            %%%%%%%%%%%%%%%%%% Soft Thresholding(Dohono,et al 1995) %%%%%%%%%%%%%%%%%
+            temp=min(sqrt(lambda),lambda/abs(S(j)));
+            ee=temp*0.1;
+            c1=abs(S(j))-ee;
+            c2=abs(c1)^2-4*(lambda-ee*abs(S(j)));
+            margin = c2;
+            
+            % Soft thresholding
+            if margin > 0
+                beta(j)=sign(S(j))*((c1+sqrt(c2))/2)/v;
+            else
+                beta(j) = 0;
+            end
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%--update r---%%%%%%%%%%%%%%%%%%%%%%%%%%
+            r=r-x_train(:,j)*(beta(j)-beta_temp(j));
+        end
+        if norm(beta_temp - beta) < (1E-5)
+            break;
+        end
+        iter=iter+1
+    end
+end
+[opt,Mse]=CV_logistic(x_train,y_train,Lambda,beta_path);
+
+beta=beta_path(:,opt);
+l_test=intercept+x_test*beta;
+test_y=sigmoid(l_test);
+l_train=intercept+x_train*beta;
+train_y=sigmoid(l_train);
+[Sensitivity_train,Specificity_train,accuracy_train] = printClassMetrics (train_y, y_train)
+[Sensitivity_test,Specificity_test,accuracy_test] = printClassMetrics (test_y, y_test)
+[Sensitivity_beta , Specificity_beta]=GetFPTP(actual_beta,beta)
+auc_train = roc_curve(train_y,y_train)
+auc_test = roc_curve(test_y,y_test)
+
